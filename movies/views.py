f3ac8e48876257d78bbcee8bfe1edea30c417209
aadideshpande <aadi.deshpande1@gmail.com>
2020-06-05 19:24:20 +0530

    some changes

--- movies/views.py
+++ movies/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
+from .forms import PostForm
 
 
 def post_list(request):
@@ -12,4 +13,8 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'post_detail.html', {'post' : post})
+
+def post_new(request):
+    form = PostForm()
+    return render(request, 'post_edit.html', {'form': form})
 
